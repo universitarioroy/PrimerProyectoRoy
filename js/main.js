5ceecb532265a87a8316171a98dfa7dcefd40de5
Edgar Huaman <rhum.sap@gmail.com>
2023-06-09 12:24:06 -0500

    Agregar .gitignore y primeros archivos

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let papa=document.querySelector('.header--title-container h1');
+console.log(papa);
+papa.addEventListener('click',function(even){
+    even.preventDefault;
+    alert ('este es un ejemplo de los eventos de lista');
+});
+
+
+
+
+
+
